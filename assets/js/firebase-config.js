c99df11c8fc5ae634cab59bf38da48e956c5180a
Vishal Kondabathini <vishal050@gmail.com>
2025-10-21 14:40:26 +0200

    Updated Configuration

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,2 @@
 // Centralized (public) Firebase configuration.
 // NOTE: This file is public when hosted — it only centralizes initialization info.
-window.firebaseConfig = {
-  apiKey: "AIzaSyDPyFt8yhwG8MDO_qqmaYMgE77xYv6u_bc",
-  authDomain: "interactcv-7b41f.firebaseapp.com",
-  projectId: "interactcv-7b41f",
-  storageBucket: "interactcv-7b41f.appspot.com",
-  messagingSenderId: "294550060415",
-  appId: "1:294550060415:web:37bf78cbccafbcaacaae4a"
-};
